9ff01591d16d71cb2d320f997534087011feaf18
Isaac <luzisaac24@gmail.com>
2025-12-02 18:30:10 -0300

    test

--- exercicio/4comparacao.js
+++ exercicio/4comparacao.js
@@ -7,3 +7,5 @@ console.log(n1 == n2 &&  n2 <= n1);
 console.log(Isaac === JavaScript || Isaac != JavaScript);
 console.log(!(n1 > n2));
 console.log(!(Isaac === "Isaac"));
+
+// continuar da unidade 4
