7bfaf818804cd00290ad2d18b39ad2e41516882b
Isaac <luzisaac24@gmail.com>
2026-02-26 17:04:25 -0300

    fix

---
+++ 16.1_shift-unshift/script.js
@@ -0,0 +1,11 @@
+let carros = ["bmw", "audi", "vw", "fiat"];
+
+let removerPrimeiroCarro = carros.shift();
+
+console.log(removerPrimeiroCarro);
+console.log(carros); 
+
+carros.unshift("Gurgel");
+
+console.log(carros);
+console.log(carros[0]);
