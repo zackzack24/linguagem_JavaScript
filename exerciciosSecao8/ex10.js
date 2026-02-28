dd3034e732a41f8db3598990a82c2e85397bf96d
Isaac <luzisaac24@gmail.com>
2026-02-28 14:56:49 -0300

    unidade 8

--- exerciciosSecao8/ex10.js
+++ exerciciosSecao8/ex10.js
@@ -26,4 +26,4 @@ const calculadora = {
 console.log(calculadora.somar(5, 5));
 console.log(calculadora.subtrair(5, 5));
 console.log(calculadora.dividir(5, 5));
-console.log(calculadora.multiplicar(5, 5));
+console.log(calculadora.multiplicar(5, 5));     
